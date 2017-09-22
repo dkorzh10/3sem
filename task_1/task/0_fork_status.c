8b8699649106ad4c12b6842adfe8b0619e0c2eec
dkorzh10 <dkorzh10@gmail.com>
2017-09-22 23:38:40 +0300

    Home tasks for 23_09

---
+++ task_1/task/0_fork_status.c
@@ -0,0 +1,42 @@
+/* Read the "4_exec_dir.c" carefully and correct the mistakes. Read about fork() return codes. */
+/* 1) build and examine this code
+   2) correct the mistake: it is possible to hang on wait. Why and when? (see "man fork")
+ */
+
+#include <unistd.h>
+#include <sys/wait.h>
+#include <stdio.h>
+#include <string.h>
+ #include <stdlib.h>
+#define  N 1000
+int main() {
+	pid_t pid;
+   	int status;
+   	int fin;
+   //	char fin[N];
+   	fin=0;
+   	//char *fin1="quit";
+   	//char *fin2;
+   //	int compareresult;
+   do	
+{
+   	switch(pid = fork()) {
+   		case(-1):
+   		    printf("error");
+   		    exit(-1);
+   		    
+   		    case 0: 
+			execl("/bin/ls","ls","-l",NULL); // this is the code the child runs 
+   		break;
+   		
+   		
+   		default: 
+			wait(&status); // this is the code the parent runs
+   	}
+   	printf("if you want to exit, press '5' , else press another nuber\n");
+   	scanf("%d",&fin);
+}
+//compareresult=srtcmp(fin1,fin2);
+while(fin!=5);
+	return 0;
+}
